f2892c2aacb3c23340635d4fb64c61b935fe7a8d
Nedim <nkalajdzij1@etf.unsa.ba>
2023-10-21 20:51:03 +0200

    Vjezba 1, zadatak 1.

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,15 @@
 package ba.unsa.etf.rpr;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int x,y;
+    Scanner unos=new Scanner(System.in);
+    System.out.println("Unesite prvi broj: ");
+    x=unos.nextInt();
+    System.out.println("Unesite prvi broj: ");
+    y=unos.nextInt();
+    int k=x+y;
+    System.out.println("Zbir dva unesena broja je: "+k);
     }
 }
